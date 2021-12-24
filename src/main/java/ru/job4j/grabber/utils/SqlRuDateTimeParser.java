9662e77f1fda53c6eaab6453958ffb8b7d668512
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-24 18:29:34 +0300

    2.1. Преобразование даты [#289476 #243601]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -32,17 +32,17 @@ public class SqlRuDateTimeParser implements DateTimeParser {
             entry("дек", "12")
     );
 
-    private static final String DATEFORMAT = "d M y, H:m";
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("d M y, H:m");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d M y");
 
     @Override
     public LocalDateTime parse(String parse) throws ParseException {
         if (parse.contains("сегодня")) {
-            parse = parse.replace("сегодня", LocalDate.now().format(DateTimeFormatter.ofPattern("d M y")));
+            parse = parse.replace("сегодня", LocalDate.now().format(DATE_TIME_FORMATTER));
         } else if (parse.contains("вчера")) {
-            parse = parse.replace("вчера", LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("d M y")));
+            parse = parse.replace("вчера", LocalDate.now().minusDays(1).format(DATE_TIME_FORMATTER));
         }
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATEFORMAT);
-        Date date = simpleDateFormat.parse(monthConvert(parse));
+        Date date = SIMPLE_DATE_FORMAT.parse(monthConvert(parse));
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
@@ -59,11 +59,11 @@ public class SqlRuDateTimeParser implements DateTimeParser {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
-        SqlRuDateTimeParser sqlparser = new SqlRuDateTimeParser();
+        SqlRuDateTimeParser sqlParser = new SqlRuDateTimeParser();
         for (Element td : row) {
             Element href = td.child(0);
             Element parent = td.parent();
-            System.out.println(sqlparser.parse(parent.child(5).text()).toString());
+            System.out.println(sqlParser.parse(parent.child(5).text()).toString());
             System.out.println(href.attr("href"));
             System.out.println(href.text());
         }
