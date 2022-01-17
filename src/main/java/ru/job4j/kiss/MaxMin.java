87981e854e89f3db3d151921e973d47681f5bfcb
Evgeny Koregin <mnemonic78@yandex.ru>
2022-01-17 14:40:01 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #251925]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,25 +2,24 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T maxElement = value.get(0);
-        for (T element : value) {
-            if (comparator.compare(element, maxElement) > 0) {
-                maxElement = element;
-            }
-        }
-        return maxElement;
+        return minMaxSearch(value, (element, minElement) -> comparator.compare(element, minElement) > 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        T minElement = value.get(0);
+        return minMaxSearch(value, (element, minElement) -> comparator.compare(element, minElement) < 0);
+    }
+
+    private <T> T minMaxSearch(List<T> value, BiPredicate<T, T> biPredicate) {
+        T minmaxElement = value.get(0);
         for (T element : value) {
-            if (comparator.compare(element, minElement) < 0) {
-                minElement = element;
+            if (biPredicate.test(element, minmaxElement)) {
+                minmaxElement = element;
             }
         }
-        return minElement;
+        return minmaxElement;
     }
 }
