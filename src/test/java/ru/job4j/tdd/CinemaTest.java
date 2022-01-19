88ea1711490b428c029a68978c2fdf38ce46d5d1
Evgeny Koregin <mnemonic78@yandex.ru>
2022-01-19 10:57:19 +0300

    2. Что такое TDD? [#4918 #252020]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tdd;
 
-import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -10,8 +12,10 @@ import static org.junit.Assert.*;
 
 public class CinemaTest {
 
-    /*
-    @Test
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Ignore
     public void whenBuy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -21,7 +25,7 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
-    @Test
+    @Ignore
     public void whenFind() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
@@ -29,24 +33,14 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
-    @Test
+    @Ignore
     public void whenNotFindSession() {
         Cinema cinema = new Cinema3D();
         List<Session> sessions = cinema.find(session -> false);
         assertNull(sessions);
     }
 
-    @Test
-    public void whenNoTicket() {
-        Account account = new AccountCinema();
-        Cinema cinema = new Cinema3D();
-        Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 00);
-        Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertNull(ticket);
-    }
-
-    @Test
+    @Ignore
     public void whenAdd() {
         Cinema cinema = new Cinema3D();
         Session session3D = new Session3D();
@@ -54,5 +48,36 @@ public class CinemaTest {
         cinema.add(session3D);
         assertThat(sessions, is(cinema.find(session -> true)));
     }
-     */
+
+    @Ignore
+    public void invalidPlace() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("This place is invalid");
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        cinema.buy(account, 10, 125, date);
+    }
+
+    @Ignore
+    public void invalidDate() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Date is invalid");
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+    }
+
+    @Ignore
+    public  void whenPlaceIsBusy() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("This place is busy");
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        cinema.buy(account, 1, 1, date);
+    }
 }
